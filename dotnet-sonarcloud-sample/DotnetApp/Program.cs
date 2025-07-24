3eec8315ab7c0dfc946de64e1cb5b799512dc7df
vishnukowshik <vishnukowshik@outlook.com>
2025-07-24 15:08:25 +0530

    added folders and files of dotnet

---
+++ dotnet-sonarcloud-sample/DotnetApp/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DotnetApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Add(2, 3));
+        }
+
+        public static int Add(int a, int b) => a + b;
+    }
+}
